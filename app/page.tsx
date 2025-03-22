8732dcab03c651188966ad8167d2904cfb80f696
shivforevershakti <shivforevershakti@gmail.com>
2025-03-22 13:34:36 +0530

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -4,36 +4,34 @@
 
 import { useEffect, useRef, useState } from "react";
 
-const Home : React.FC =() => {
+const Home: React.FC = () => {
   const [duration, setDuration] = useState<number>(5);
   const [running, setRunning] = useState<boolean>(false);
   const [progress, setProgress] = useState<number>(0);
   const [color, setColor] = useState<string>('#5caf50');
-  const [startTime , setStartTime] = useState<number | null>(null);
-  const [elapsed , setElapsed] = useState<number>(0);
-
-  const intervalref = useRef<any>(null)
-
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [elapsed, setElapsed] = useState<number>(0);
+  const intervalref = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     if (running) {
-     intervalref.current = setInterval(() =>{
-      const timeElapsed = Date.now() - (startTime || 0);
-      const newProgress = Math.min((timeElapsed / (duration * 1000)) * 100 , 100);
-      setProgress(newProgress);
-      if(newProgress >=100){
-        clearInterval(intervalref.current);
-        setRunning(false);
-      }
-     } , 100)
+      intervalref.current = setInterval(() => {
+        const timeElapsed = Date.now() - (startTime || 0);
+        const newProgress = Math.min((timeElapsed / (duration * 1000)) * 100, 100);
+        setProgress(newProgress);
+        if (newProgress >= 100) {
+          clearInterval(intervalref.current!);
+          setRunning(false);
+        }
+      }, 100)
 
-    }else{
-      clearInterval(intervalref.current);
+    } else {
+      clearInterval(intervalref.current!);
     }
 
-    return () => clearInterval(intervalref.current)
-  
-  }, [running, duration])
+    return () => clearInterval(intervalref.current!)
+
+  }, [running, duration, startTime])
 
   const handleStartProgress = () => {
     setRunning(true);
@@ -54,7 +52,7 @@ const Home : React.FC =() => {
 
 
       <div className="w-full bg-gray-200 rounded overflow-hidden h-8 mt-10">
-        <div className="h-full transition-all" style={{ width: `${progress}%`, background: color ,  }}></div>
+        <div className="h-full transition-all" style={{ width: `${progress}%`, background: color, }}></div>
 
       </div>
       <p>{Math.round(progress)}%</p>
